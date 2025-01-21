199d0a414d570a0071b0891d306d9f5d9498012e
daniel <danielgneves.contato@gmail.com>
2025-01-21 14:58:33 -0400

    menu

--- src/me/daniel/academia/factory/ConnectionFactory.java
+++ src/me/daniel/academia/factory/ConnectionFactory.java
@@ -6,11 +6,9 @@ import java.sql.DriverManager;
 public class ConnectionFactory {
 
 	private static final String USERNAME = "root";
-<<<<<<< HEAD
+	
 	private static final String PASSWORD = "";
-=======
-	private static final String PASSWORD = "326435";
->>>>>>> bc1c59553f9f015ad2b84291da4250f2b39a4835
+
 	private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/academia";
 
 	public static Connection conectar() throws Exception {
